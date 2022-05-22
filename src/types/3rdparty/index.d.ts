92380e12e5a64b4aca668d201867ac2918f834cb
kalush81 <krzysiek.kaluza@gmail.com>
2022-05-22 15:21:32 +0200

    Base configuration, setup and structure

---
+++ src/types/3rdparty/index.d.ts
@@ -0,0 +1,15 @@
+// in this file you will be adding missing type definitions
+
+import _ from "lodash";
+
+declare module "lodash" {
+  interface LoDashStatic {
+    /**
+     * Multiply two numbers.
+     * @param multiplier The first number in a multiplication.
+     * @param multiplicand The second number in a multiplication.
+     * @returns Returns the product.
+     */
+    multiply(multiplier: number, multiplicand: number): number;
+  }
+}
---
+++ src/withLodash.ts
@@ -0,0 +1,3 @@
+import _ from "lodash";
+
+const res = _.multiply(2, 3);
