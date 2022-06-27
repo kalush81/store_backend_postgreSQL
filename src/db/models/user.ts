6c87b32ea33fcfb4aef851824c82cef257e4bbb4
kalush81 <krzysiek.kaluza@gmail.com>
2022-06-27 15:44:18 +0200

    Update User model

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -1,21 +1,21 @@
 //user model
-import pool from ".."
+import pool from "..";
 
 export type User = {
-  email: string,
-  password: string
-}
+  email: string;
+  password: string;
+};
 
-export class UserStore { 
-
-  async create(): Promise<User> { 
-    const conn = await pool.connect()
-    const sql = 'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id;'
-    return {email: '', password: ''}
+export class UserStore {
+  async create(u: User): Promise<User> {
+    const conn = await pool.connect();
+    const sql =
+      "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id;";
+    const result = await conn.query(sql, [u.email, u.password]);
+    return result.rows[0];
   }
 
-  async authenticate() { 
-
+  async authenticate() {
+    //to be implemented for login
   }
-
 }
